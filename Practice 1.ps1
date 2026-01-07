57037176ef1de8acf9b9e775232741d5e0b0c0b3
Grex-jpg <gregor.hein@voco.ee>
2026-01-07 13:43:38 +0200

    Practice1 on tehtud

---
+++ Practice 1.ps1
@@ -0,0 +1,17 @@
+﻿#Siin proovib aru saada kuidas kasutada $nameArray-ed
+#Alguses paneb kirja "shotcutid"
+
+$arry1 = @(1,2,3)
+$arry2 = @(4,5,6)
+$arry3 = @()
+
+#Siis saab neid kasutada arvutamisel et saada arry3 vastuseks 5 7 ja 9
+
+$arry3 += $arry1[0] + $arry2[0]
+$arry3 += $arry1[1] + $arry2[1]
+$arry3 += $arry1[2] + $arry2[2]
+
+
+$arry3
+
+pause
