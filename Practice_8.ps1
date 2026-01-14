83eec67d2fe9288ddb4f0ad37e043b8d28fd298b
Grex-jpg <gregor.hein@voco.ee>
2026-01-14 15:13:04 +0200

    Practice8 on tehtud

---
+++ Practice_8.ps1
@@ -0,0 +1,15 @@
+﻿do {
+
+    $notepads = Get-Process -Name "notepad" -ErrorAction SilentlyContinue
+
+    if ($notepads) {
+
+        Write-Host "Notepad is running"
+    } else {
+
+        Write-Host "Notepad has stopped running"
+    }
+
+    Start-Sleep -Seconds 1
+
+} while ($notepads)
