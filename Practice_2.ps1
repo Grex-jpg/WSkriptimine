247fb867b8019f3a6fe813ef390a600c4a3c2115
Grex-jpg <gregor.hein@voco.ee>
2026-01-14 12:56:10 +0200

    Practice2 on tehtud

---
+++ Practice_2.ps1
@@ -0,0 +1,16 @@
+﻿$tööpäevad=@{John=12; Marry=20; Joe=18}
+
+$päevapalk=@{John=100; Marry=120; Joe=150}
+
+$salary = @{}
+
+foreach ($name in $tööpäevad.Keys) {
+    $salary[$name] = $tööpäevad[$name] * $päevapalk[$name]
+}
+
+$salary.GetEnumerator() | ForEach-Object {
+    [PSCustomObject]@{
+        Name   = $_.Key
+        Salary = $_.Value
+    }
+}
