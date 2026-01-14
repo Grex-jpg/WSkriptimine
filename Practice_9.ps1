7f8e9e446cd6c9deba74415572ef05c74c3eaa59
Grex-jpg <gregor.hein@voco.ee>
2026-01-14 15:13:24 +0200

    Practice9 on tehtud

---
+++ Practice_9.ps1
@@ -0,0 +1,17 @@
+﻿$count = 0
+
+do {
+    # Get all running Notepad processes
+    $notepads = Get-Process -Name "notepad" -ErrorAction SilentlyContinue
+
+    if ($notepads) {
+        Write-Host "Notepad is running"
+        $count++
+    }
+
+    Start-Sleep -Seconds 1
+
+} while ($notepads) 
+
+Write-Host "All Notepad instances have stopped running."
+Write-Host "'Notepad is running' was displayed $count times."
