e38e119e9d113955bc92a38dfb3b4c6f6269e3f1
Grex-jpg <gregor.hein@voco.ee>
2026-01-14 15:12:35 +0200

    Practice6 on tehtud

---
+++ Practice_6.ps1
@@ -0,0 +1,21 @@
+﻿$info = Import-Csv C:\Users\Grexx\wskriptimine\Info.csv
+
+$studentsWithSchool = $info | ForEach-Object {
+    $school = if ([int]$_.Age -ge 4 -and [int]$_.Age -le 10) {
+        "Junior"
+    }
+    elseif ([int]$_.Age -ge 11 -and [int]$_.Age -le 17) {
+        "Senior"
+    }
+    else {
+        "Unknown"
+    }
+
+    [PSCustomObject]@{
+        Name   = $_.Name
+        School = $school
+    }
+}
+$studentsWithSchool | Format-Table
+
+$studentsWithSchool | Export-Csv -Path "C:\Users\Grexx\wskriptimine\infovastus.txt" -NoTypeInformation
