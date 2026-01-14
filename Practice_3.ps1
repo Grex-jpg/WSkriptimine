3988c05fa3d52c070aed7e5bf6386273bbed23d2
Grex-jpg <gregor.hein@voco.ee>
2026-01-14 13:33:33 +0200

    Practice3 on tehtud ja ma tegin veel paar lisa asja :D

---
+++ Practice_3.ps1
@@ -0,0 +1,7 @@
+﻿Write-Host "Tervist!"
+sleep 1
+Write-Host "Mis on teie nimi? :" -ForegroundColor Red -NoNewline
+$nimi = Read-Host
+Write-Host "Tere " -NoNewline
+Write-Host "$nimi " -ForegroundColor Cyan -NoNewline
+Write-Host "tore tutvuda, ma olen Practice_3 :D"
