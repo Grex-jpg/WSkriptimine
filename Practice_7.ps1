a950233d226833f4ad7d7e063a62cf562801bbb6
Grex-jpg <gregor.hein@voco.ee>
2026-01-14 15:12:52 +0200

    Practice7 on tehtud

---
+++ Practice_7.ps1
@@ -0,0 +1,11 @@
+﻿$colors = @("Red", "Green", "Yellow", "Blue")
+$students = 1..20
+
+$studentGroups = $students | ForEach-Object {
+    [PSCustomObject]@{
+        RollNumber = $_
+        Group = $colors | Get-Random
+    }
+}
+
+$studentGroups | Format-Table
