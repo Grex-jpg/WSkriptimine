dcd3fb4680aa8496efa31729bd1119630023a708
Grex-jpg <gregor.hein@voco.ee>
2026-01-14 14:13:34 +0200

    Practice4 mõlemad ii-d on tehtud

---
+++ Practice_4i.ps1
@@ -0,0 +1,15 @@
+﻿$essa = Read-Host "Kirjutage mingi number"
+$essa = [double]$essa
+
+$teine = Read-Host "Nooo pane üks veel kirja"
+$teine = [double]$teine
+
+if ($essa -gt $teine) {
+    Write-Host "Suurem number on! $essa"
+}
+elseif ($teine -gt $essa) {
+    Write-Host "Suurem number on! $teine"
+}
+else {
+    Write-Host "Mõlemad on võrdsed lihtsalt lihtsalt :("
+}
---
+++ Practice_4ii.ps1
@@ -0,0 +1,30 @@
+﻿Write-Host "Vali riik! :" -ForegroundColor Green
+Write-Host "[1] Eesti"
+Write-Host "[2] Saksamaa"
+Write-Host "[3] Hiina" 
+$valik = [int](Read-Host "Valik üks nendest riikidest")
+
+if($valik -eq 1)
+{
+Write-Host "Hea valik siin on veike fackt - " -NoNewline
+sleep 1
+Write-Host "Eesti kapitali on Tallinn."
+}
+
+elseif($valik -eq 2)
+{
+Write-Host "Hea valik siin on veike fackt - " -NoNewline
+sleep 1
+Write-Host "Saksamaa kapitali on Berliin."
+}
+
+elseif($valik -eq 3)
+{
+Write-Host "Hea valik siin on veike fackt - " -NoNewline
+sleep 1
+Write-Host "Hiina kapitali on Beijing."
+}
+else
+{
+Write-Host "Mees... sellist valikut pole 1,2 ja 3 aint!" -ForegroundColor Red
+}
