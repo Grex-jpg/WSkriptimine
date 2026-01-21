1320d41e0b4bd9e2f9d94eb0c48c551f919f8ba4
Grex-jpg <gregor.hein@voco.ee>
2026-01-21 09:35:34 +0200

    ÜL1 on tehtud

---
+++ ÜL1_Konto_Loomine_eesjaperenimega.ps1
@@ -0,0 +1,34 @@
+﻿# Küsi kasutaja andmed
+$eesnimi = Read-Host "Palun sisesta oma eesnimi"
+$perenimi = Read-Host "Palun sisesta oma perekonnanimi"
+
+# Loo kasutajanimi ja muud andmed
+$kasutajanimi = ("$eesnimi.$perenimi").ToLower()
+$taisnimi = "$eesnimi $perenimi"
+$kirjeldus = "Kasutaja $taisnimi"
+
+# Kontrolli, kas kasutaja juba eksisteerib
+$olemas = Get-LocalUser -Name $kasutajanimi -ErrorAction SilentlyContinue
+
+if ($olemas) {
+    Write-Host "See kasutaja $kasutajanimi on juba olemas" -ForegroundColor Yellow
+}
+else {
+    # Määra parool
+    $parool = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+
+    try {
+        New-LocalUser `
+            -Name $kasutajanimi `
+            -Password $parool `
+            -FullName $taisnimi `
+            -Description $kirjeldus `
+            -ErrorAction Stop
+
+        Write-Host "Kasutaja nimega $kasutajanimi loodi edukalt" -ForegroundColor Green
+    }
+    catch {
+        Write-Host "VIGA kasutaja loomisel:" -ForegroundColor Red
+        Write-Host $_.Exception.Message -ForegroundColor Red
+    }
+}
