d72ea6e97c469c60594c2a4279338e7546369ded
Grex-jpg <gregor.hein@voco.ee>
2026-01-28 10:08:26 +0200

    Practice12i on tehtud

---
+++ Practice_12i.ps1
@@ -0,0 +1,9 @@
+﻿function countstart {
+    $count = (Get-Service | Where-Object {$_.Status -eq "Running"}).Count
+    Write-Host "Töötavate teenuste koguarv = $count"
+}
+
+function countstop {
+    $count = (Get-Service | Where-Object {$_.Status -eq "Stopped"}).Count
+    Write-Host "Peatatud olekus teenuste koguarv = $count"
+}
