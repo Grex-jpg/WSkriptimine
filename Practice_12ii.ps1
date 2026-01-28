6df15ee1b536f0cf2513256941355251864b575c
Grex-jpg <gregor.hein@voco.ee>
2026-01-28 10:08:41 +0200

    Practice12ii on tehtud

---
+++ Practice_12ii.ps1
@@ -0,0 +1,12 @@
+﻿#Kuidas kävitada :
+#countservice -status Running
+#countservice -status Stopped
+
+function countservice {
+    param (
+        [string]$status
+    )
+
+    $count = (Get-Service | Where-Object {$_.Status -eq $status}).Count
+    Write-Host "Total services in $status state = $count"
+}
